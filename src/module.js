b443cf0de34ff8950900ee640b7ad660d62630ac
Hayes Neuman <neurosie@gmail.com>
2019-12-23 21:31:45 -0500

    Set up basic layout + printing

--- src/module.js
+++ src/module.js
@@ -1,23 +1,38 @@
-var statusElement = document.getElementById('status');
-var progressElement = document.getElementById('progress');
-var spinnerElement = document.getElementById('spinner');
-var errorBuffer = "";
+const statusElement = document.getElementById('status');
+const progressElement = document.getElementById('progress');
+const spinnerElement = document.getElementById('spinner');
+const outputElement = document.getElementById('output');
+
+const outputBuffers = {
+  'stdout': '',
+  'stderr': '',
+};
+
+function printChar (device) {
+  return (c) => {
+    if (c == '\n'.charCodeAt(0)) {
+      let str = outputBuffers[device];
+      const block = document.createElement(str.length ? 'div' : 'br');
+      block.classList.add('block', device);
+      block.innerHTML = str;
+      outputElement.append(block);
+      outputBuffers[device] = '';
+    } else {
+      outputBuffers[device] += String.fromCharCode(c);
+    }
+  }
+}
 
 var Module = {
   preRun: [() => {
-    FS.init(() => {
-      // input
-      return null;
-    }, (a) => {
-      // console.log(a);
-      document.getElementById("output").value += String.fromCharCode(a);
-    }, (a) => {
-      if (a == '\n'.charCodeAt(0)) {
-        console.error(errorBuffer);
-      } else {
-        errorBuffer += String.fromCharCode(a);
-      }
-    });
+    FS.init(
+      () => {
+        // input
+        return null;
+      }, 
+      printChar('stdout'),
+      printChar('stderr'),
+    );
   }],
   postRun: [],
   // print: (function() {
@@ -51,44 +66,44 @@ var Module = {
 
   //   return canvas;
   // })(),
-  setStatus: function(text) {
-    if (!Module.setStatus.last) Module.setStatus.last = { time: Date.now(), text: '' };
-    if (text === Module.setStatus.last.text) return;
-    var m = text.match(/([^(]+)\((\d+(\.\d+)?)\/(\d+)\)/);
-    var now = Date.now();
-    if (m && now - Module.setStatus.last.time < 30) return; // if this is a progress update, skip it if too soon
-    Module.setStatus.last.time = now;
-    Module.setStatus.last.text = text;
-    if (m) {
-      text = m[1];
-      progressElement.value = parseInt(m[2])*100;
-      progressElement.max = parseInt(m[4])*100;
-      progressElement.hidden = false;
-      spinnerElement.hidden = false;
-    } else {
-      progressElement.value = null;
-      progressElement.max = null;
-      progressElement.hidden = true;
-      if (!text) spinnerElement.style.display = 'none';
-    }
-    statusElement.innerHTML = text;
-  },
+  // setStatus: function(text) {
+  //   if (!Module.setStatus.last) Module.setStatus.last = { time: Date.now(), text: '' };
+  //   if (text === Module.setStatus.last.text) return;
+  //   const m = text.match(/([^(]+)\((\d+(\.\d+)?)\/(\d+)\)/);
+  //   const now = Date.now();
+  //   if (m && now - Module.setStatus.last.time < 30) return; // if this is a progress update, skip it if too soon
+  //   Module.setStatus.last.time = now;
+  //   Module.setStatus.last.text = text;
+  //   if (m) {
+  //     text = m[1];
+  //     progressElement.value = parseInt(m[2])*100;
+  //     progressElement.max = parseInt(m[4])*100;
+  //     progressElement.hidden = false;
+  //     spinnerElement.hidden = false;
+  //   } else {
+  //     progressElement.value = null;
+  //     progressElement.max = null;
+  //     progressElement.hidden = true;
+  //     if (!text) spinnerElement.style.display = 'none';
+  //   }
+  //   statusElement.innerHTML = text;
+  // },
   // totalDependencies: 0,
   // monitorRunDependencies: function(left) {
   //   this.totalDependencies = Math.max(this.totalDependencies, left);
   //   Module.setStatus(left ? 'Preparing... (' + (this.totalDependencies-left) + '/' + this.totalDependencies + ')' : 'All downloads complete.');
   // }
 };
-Module.setStatus('Downloading...');
-window.onerror = function(event) {
-  // TODO: do not warn on ok events like simulating an infinite loop or exitStatus
-  Module.setStatus('Exception thrown, see JavaScript console');
-  spinnerElement.style.display = 'none';
-  Module.setStatus = function(text) {
-    if (text) Module.printErr('[post-exception status] ' + text);
-  };
-};
+// Module.setStatus('Downloading...');
+// window.onerror = function(event) {
+//   // TODO: do not warn on ok events like simulating an infinite loop or exitStatus
+//   Module.setStatus('Exception thrown, see JavaScript console');
+//   spinnerElement.style.display = 'none';
+//   Module.setStatus = function(text) {
+//     if (text) Module.printErr('[post-exception status] ' + text);
+//   };
+// };
 
-var script = document.createElement('script');
-script.src = "wander.js";
+const script = document.createElement('script');
+script.src = 'wander.js';
 document.body.appendChild(script);
